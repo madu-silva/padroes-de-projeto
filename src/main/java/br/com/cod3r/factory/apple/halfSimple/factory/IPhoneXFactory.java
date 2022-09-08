6081d48e6c590ce4090cbe03655e0123c7b43f77
Maria Eduarda <mariaeduarda050501@gmail.com>
2022-09-08 17:04:55 -0300

    Padrão Factory implementado de uma segunda maneira

---
+++ src/main/java/br/com/cod3r/factory/apple/halfSimple/factory/IPhoneXFactory.java
@@ -0,0 +1,16 @@
+package br.com.cod3r.factory.apple.halfSimple.factory;
+
+import br.com.cod3r.factory.apple.halfSimple.model.IPhone;
+import br.com.cod3r.factory.apple.halfSimple.model.IPhoneX;
+import br.com.cod3r.factory.apple.halfSimple.model.IPhoneXSMax;
+
+public class IPhoneXFactory extends IPhoneFactory {
+
+    public IPhone createIphone(String level) {
+        if (level.equals("standard")) {
+            return new IPhoneX();
+        } else if (level.equals("highEnd")) {
+            return new IPhoneXSMax();
+        } else return null;
+    }
+}

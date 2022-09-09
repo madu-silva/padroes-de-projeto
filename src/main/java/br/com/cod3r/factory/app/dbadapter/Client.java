f360aa15ce9e35f4b357a03a72c6907b0ddcccf7
Maria Eduarda <mariaeduarda050501@gmail.com>
2022-09-09 14:46:04 -0300

    Padrão Factory implementado em um outro exemplo

--- src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,15 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.factory.OracleFactory;
+import br.com.cod3r.factory.app.dbadapter.factory.PostgresFactory;
+
 public class Client {
 
 	public static void main(String[] args) {
-
+//		DB db = new PostgresFactory().getDatabase();
+		DB db = new OracleFactory().getDatabase();
+		db.query("SELECT * FROM users");
+		db.update("INSERT INTO users VALUES ('User', 25)");
 	}
 }
